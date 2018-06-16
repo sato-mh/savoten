74a7415d2caa123018dafedd594c0500a164c2ac
JunYamaguchi <jun.smith.y@gmail.com>
2018-06-17 01:05:29 +0900

    add post test_case

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -25,3 +25,27 @@ def test_get_event(get_event_test_case):
     test_app = app.api.test_client()
     response = test_app.get(uri)
     assert response.status_code == expect
+
+@pytest.fixture(
+    scope='function',
+    params=[
+        {
+            'uri': '/events',
+            'expect': 200
+        },
+        {
+            'uri': '/events/hogehoge',
+            'expect': 405
+        }
+    ]
+)
+
+def post_event_test_case(request):
+    return request.param
+
+def test_post_event(post_event_test_case):
+    uri = post_event_test_case['uri']
+    expect = post_event_test_case['expect'] 
+    test_app = app.api.test_client()
+    response = test_app.post(uri) 
+    assert response.status_code == expect
