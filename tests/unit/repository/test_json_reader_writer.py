82ad381d6062df1f8e0d1179218a6b27d944d246
conhumi <con_humi@live.jp>
2018-06-24 14:25:54 +0900

    touch を削除しました

--- tests/unit/repository/test_json_reader_writer.py
+++ tests/unit/repository/test_json_reader_writer.py
@@ -1,10 +1,10 @@
-from savoten.repository.json_reader_writer import json_read, json_write, lock, unlock
+from savoten.repository.json_reader_writer import (
+    json_read, json_write, lock, unlock
+)
 
 import os
-import pathlib
 import json
 from filelock import FileLock
-from unittest.mock import patch
 
 _dname = os.path.dirname
 
@@ -12,12 +12,14 @@ _dname = os.path.dirname
 def test_lock():
     test_file_name = _dname(os.path.abspath(__file__)) + '/data/lock_test'
     actual = lock(test_file_name)
-    assert isinstance(actual, FileLock) 
+    assert isinstance(actual, FileLock)
+
 
 def test_unlock():
     lock = FileLock('')
     unlock(lock)
     assert lock.is_locked is False
+
 
 def test_json_read():
     test_file_name = _dname(os.path.abspath(__file__)) + '/data/json_read'
@@ -28,7 +30,6 @@ def test_json_read():
 
 def test_json_write():
     test_file_name = _dname(os.path.abspath(__file__)) + '/data/json_write'
-    pathlib.Path(test_file_name).touch()
     expect_data = json.dumps({"hoge": "fuga"})
     json_write(filename=test_file_name, data=expect_data)
     actual_data = json_read(filename=test_file_name)
