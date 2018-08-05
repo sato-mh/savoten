16563b36aa312a54b5fae829fd0a159000bb22c0
conhumi <con_humi@live.jp>
2018-08-05 21:26:06 +0900

    fix findings

--- tests/unit/repository/test_json_reader_writer.py
+++ tests/unit/repository/test_json_reader_writer.py
@@ -5,17 +5,17 @@ import os
 from filelock import FileLock
 
 _dname = os.path.dirname
-_test_read_file_name = os.path.join(
+TEST_READ_FILE_NAME = os.path.join(
         _dname(os.path.abspath(__file__)),
         'data',
         'json_read'
     )
-_test_write_file_name = os.path.join(
+TEST_WRITE_FILE_NAME = os.path.join(
         _dname(os.path.abspath(__file__)),
         'data',
         'json_write'
     )
-_test_lock_file_name = os.path.join(
+TEST_LOCK_FILE_NAME = os.path.join(
     _dname(os.path.abspath(__file__)),
     'data',
     'lock_test'
@@ -23,33 +23,33 @@ _test_lock_file_name = os.path.join(
 
 
 def teardown_function():
-    if os.path.exists(_test_lock_file_name + '.lock'):
-        os.remove(_test_lock_file_name)
-    if os.path.exists(_test_write_file_name):
-        os.remove(_test_write_file_name)
+    if os.path.exists(TEST_LOCK_FILE_NAME + '.lock'):
+        os.remove(TEST_LOCK_FILE_NAME)
+    if os.path.exists(TEST_WRITE_FILE_NAME):
+        os.remove(TEST_WRITE_FILE_NAME)
 
 
 def test_lock():
-    test_file_name = _test_lock_file_name
+    test_file_name = TEST_LOCK_FILE_NAME
     actual = lock(test_file_name)
     assert isinstance(actual, FileLock)
 
 
 def test_unlock():
-    lock = FileLock('')
+    lock = FileLock(TEST_LOCK_FILE_NAME)
     unlock(lock)
     assert lock.is_locked is False
 
 
 def test_json_read():
-    test_file_name = _test_read_file_name
+    test_file_name = TEST_READ_FILE_NAME
     expect_data = {"hoge": "fuga"}
     actual_data = json_read(filename=test_file_name)
     assert expect_data == actual_data
 
 
 def test_json_write():
-    test_file_name = _test_write_file_name
+    test_file_name = TEST_WRITE_FILE_NAME
     expect_data = {"hoge": "fuga"}
     json_write(filename=test_file_name, data=expect_data)
     actual_data = json_read(filename=test_file_name)
