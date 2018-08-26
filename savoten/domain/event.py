25756b267bb58856390175d4e24dfb1f07177ac2
sato-mh <satomh37@gmail.com>
2018-08-26 23:25:33 +0900

    Modify event to apply new interface

--- savoten/domain/event.py
+++ savoten/domain/event.py
@@ -1,11 +1,31 @@
-from savo.domain import Period
+from savo.domain import EventItem, Period
 
 
 class Event:
 
     def __init__(self, name, items, id=None, start=None, end=None,
-                 description=None):
+                 description=None, anonymous=False,
+                 created_at=None, updated_at=None, deleted_at=None):
         self.id = id
         self.name = name
+        self.items = items
         self.period = Period(start, end)
         self.description = description
+        self.anonymous = anonymous
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.deleted_at = deleted_at
+
+    @property
+    def items(self):
+        return self.items
+
+    @items.setter
+    def items(self, items):
+        if not (isinstance(items, list)
+                and all([isinstance(item, EventItem) for item in items])):
+            raise TypeError('items is required List[EventItem]. not {}.'
+                            .format(list, type(items)))
+
+    def is_within(self):
+        return self.period.is_within()
