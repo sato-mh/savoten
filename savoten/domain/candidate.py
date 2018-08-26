c4eff88a43dd60a6b2b4eaf781a5f449698fd2c7
sato-mh <satomh37@gmail.com>
2018-08-26 23:32:52 +0900

    Add validation to Candidate for user argument

--- savoten/domain/candidate.py
+++ savoten/domain/candidate.py
@@ -1,6 +1,20 @@
+from .user import User
+
+
 class Candidate:
 
     def __init__(self, user, description=None, id=None):
         self.id = id
         self.user = user
         self.description = description
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, user):
+        if not isinstance(user, User):
+            raise TypeError('items is required {}. not {}.'
+                            .format(User, type(user)))
+        self._user = user
