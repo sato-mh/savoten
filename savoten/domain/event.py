abe23ca153509a76e4a15ee9429e00e6ed3fbf0a
sato-mh <satomh37@gmail.com>
2018-08-30 01:18:28 +0900

    Modify Event to pass Period instance

--- savoten/domain/event.py
+++ savoten/domain/event.py
@@ -1,15 +1,16 @@
-from savo.domain import EventItem, Period
+from .event_item import EventItem
+from .period import Period
 
 
 class Event:
 
-    def __init__(self, name, items, id=None, start=None, end=None,
+    def __init__(self, name, items, period, id=None,
                  description=None, anonymous=False,
                  created_at=None, updated_at=None, deleted_at=None):
         self.id = id
         self.name = name
         self.items = items
-        self.period = Period(start, end)
+        self.period = period
         self.description = description
         self.anonymous = anonymous
         self.created_at = created_at
@@ -27,6 +28,17 @@ class Event:
             raise TypeError('items is required List[EventItem]. not {}.'
                             .format(type(items)))
         self._items = items
+
+    @property
+    def period(self):
+        return self._period
+
+    @period.setter
+    def period(self, period):
+        if not isinstance(period, Period):
+            raise TypeError('period is required {}. not {}.'
+                            .format(Period, type(period)))
+        self._period = period
 
     def is_within(self):
         return self.period.is_within()
