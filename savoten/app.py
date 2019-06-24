dd4bb60253a090ec0a14502d2750fbbea549a0db
JunYamaguchi <jun.smith.y@gmail.com>
2019-06-25 01:17:27 +0900

    create_eventとテストをresponder仕様に修正

--- savoten/app.py
+++ savoten/app.py
@@ -17,21 +17,25 @@ def get_event(req, resp, *, event_id):
 
     # stubの動作
     # stub応答のためダミーパラメータでeventオブジェクトを生成
+    test_event_id = 1234
     try:
+        if int(event_id) != test_event_id:
+            raise Exception("event_id does not exist.")
         start = datetime.datetime.now()
         end = start + datetime.timedelta(hours=1)
         args = {
             'id': event_id,
-            'name': 'test event_name',
+            'name': 'get_event_stub event_name',
             'items': [],
             'period': domain.Period(start, end),
             'description': 'description for test'
         }
         event = domain.Event(**args)
-    except Exception as e:
-        print('exception: %s', e)
+    except:
         resp.status_code = api.status_codes.HTTP_404
-        resp.media = {'error': 'Not found'}
+        resp.media = {'result': False,
+                      'error': 'Target event_id does not exist'}
+        return
 
     # レスポンスに使うdictを生成
     result = {
@@ -48,27 +52,38 @@ def get_event(req, resp, *, event_id):
     # レスポンス
     resp.media = result
 
+
 '''
-@api.route('/events', methods=['POST'])
-def create_event():
-
-    print(events)
-    event_id = len(events) + 1
-    start = datetime.datetime.now()
-    end = start + datetime.timedelta(hours=24)
-
-    args = {
-        'name': event_id,
-        'items': [],
-        'period': domain.Period(start, end),
-        'description': 'description for test'
-    }
-    event = domain.Event(**args)
-
-    events[event_id] = event
-
-    return make_response(f'create event{vars(event)})!', 201)
+create_event
+/eventにPOSTされたときのbodyからeventのパラメータを抽出
+既存eventの総数を1インクリメントした値を新しいevent_idとしてセット
+eventsに登録する
 '''
+@api.route('/events')
+class Events():
+    async def on_post(self, req, resp):
+        try:
+            request = await req.media()
+            event_id = len(events) + 1
+            start = datetime.datetime.now()
+            end = start + datetime.timedelta(hours=24)
+            args = {
+                'id': event_id,
+                'name': request['name'],
+                'items': [],
+                'period': domain.Period(start, end),
+                'description': 'description for test'
+            }
+            event = domain.Event(**args)
+            events[event_id] = event
+        except:
+            resp.status_code = api.status_codes.HTTP_400
+            resp.media = {'result': False, 'error': 'create_event failed.'}
+            return
+
+        resp.status_code = api.status_codes.HTTP_201
+        resp.media = {'result': True}
+
 
 if __name__ == '__main__':
     api.run(address='0.0.0.0')
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -6,16 +6,17 @@ from savoten import app
     scope='function',
     params=[
         {
+            # 存在する想定のID
             'uri': '/events/1234',
             'expect': 200
         },
         {
-            'uri': '/events/',
+            # 存在しない想定のID
+            'uri': '/events/9999',
             'expect': 404
         }
     ]
 )
-
 def get_event_test_case(request):
     return request.param
 
@@ -27,17 +28,27 @@ def test_get_event(get_event_test_case):
     response = test_app.requests.get(uri)
     assert response.status_code == expect
 
-'''
+
 @pytest.fixture(
     scope='function',
     params=[
         {
             'uri': '/events',
+            'data': {
+                'name': 'create_event stub event_name',
+                'items': [],
+                'description': ''
+            },
             'expect': 201
         },
         {
-            'uri': '/events/hogehoge',
-            'expect': 405
+            'uri': '/events',
+            'data': {
+                'name': None,
+                'items': [],
+                'description': ''
+            },
+            'expect': 400
         }
     ]
 )
@@ -48,7 +59,7 @@ def post_event_test_case(request):
 def test_post_event(post_event_test_case):
     uri = post_event_test_case['uri']
     expect = post_event_test_case['expect']
-    test_app = app.api.test_client()
-    response = test_app.post(uri)
+    data = post_event_test_case['data']
+    test_app = app.api
+    response = test_app.requests.post(uri, data)
     assert response.status_code == expect
-'''
