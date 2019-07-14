723c5721381d690ab17fa0eadec17eef17503d49
JunYamaguchi <jun.smith.y@gmail.com>
2019-07-14 17:31:10 +0900

    fix app.py create_event

--- savoten/app.py
+++ savoten/app.py
@@ -1,5 +1,6 @@
 import datetime
-from flask import Flask, jsonify, abort, make_response
+import dateutil.parser
+from flask import Flask, jsonify, abort, make_response, request
 from savoten import domain
 
 api = Flask(__name__)
@@ -39,28 +40,31 @@ def get_event(event_id):
 @api.route('/events', methods=['POST'])
 def create_event():
 
-    print(events)
-    event_id = len(events) + 1
-    start = datetime.datetime.now()
-    end = start + datetime.timedelta(hours=24)
+    try:
+        args = request.json
 
-    args = {
-        'name': event_id,
-        'items': [],
-        'period': domain.Period(start, end),
-        'description': 'description for test'
-    }
-    event = domain.Event(**args)
+        event_id = len(events) + 1
+        args['id'] = event_id
 
-    events[event_id] = event
+        start = dateutil.parser.parse(args['start'])
+        end = dateutil.parser.parse(args['end'])
+        period = domain.Period(start, end),
+        args['period'] = period
+        del args['start'], args['end']
+
+        args['items'] = []
+
+        event = domain.Event(**args)
+        events[event_id] = event
+    except Exception as e:
+        api.logger.error ('create_event fail: %s'% e)
+        return('create_event fail.', 400)
 
     return make_response(f"create event{vars(event)})!", 201)
-
 
 @api.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=8000)
