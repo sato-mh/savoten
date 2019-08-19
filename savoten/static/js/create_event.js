8d8bf70ed17ae6169cabd371739830849dde8d87
JunYamaguchi <jun.smith.y@gmail.com>
2019-08-19 23:44:46 +0900

    rename btn to button

--- savoten/static/js/create_event.js
+++ savoten/static/js/create_event.js
@@ -1,46 +1,56 @@
-const setPostJsonEventToCreateEventBtn = () => {
-  const create_event_btn = document.getElementById('create_event_btn')
-  create_event_btn.addEventListener('click', () => {
-    const requestData = formDataToJson(document.forms['create_event_form'])
-    postJson(requestData);
-  }, false);
-}
+const setPostJsonEventToCreateEventButton = () => {
+  const create_event_button = document.getElementById("create_event_button");
+  create_event_button.addEventListener(
+    "click",
+    () => {
+      const requestData = formDataToJson(document.forms["create_event_form"]);
+      postJson(requestData);
+    },
+    false
+  );
+};
 
-const formDataToJson = (form) => {
+const formDataToJson = form => {
   const formData = $(form).serializeArray();
   const jsonData = parseJson(formData);
-  return jsonData
-}
+  return jsonData;
+};
 
-const parseJson = (data) => {
+const parseJson = data => {
   const returnJson = {};
   for (let i = 0; i < data.length; i++) {
-    returnJson[data[i].name] = data[i].value
+    returnJson[data[i].name] = data[i].value;
   }
   return returnJson;
-}
+};
 
-const postJson = (jsonData) => {
+const postJson = jsonData => {
   $.post({
     type: "post",
     url: "/api/v1/events",
     data: JSON.stringify(jsonData),
-    contentType: 'application/json',
-  }).then((responseData) => {
-    alert('Create event success!! [event_id: ' + responseData['id'] + ']');
-    console.log(responseData);
-    return;
-  },
+    contentType: "application/json"
+  }).then(
+    responseData => {
+      alert("Create event success!! [event_id: " + responseData["id"] + "]");
+      console.log(responseData);
+      return;
+    },
     (xhr, textStatus, errorThrown) => {
       try {
         const responseData = $.parseJSON(xhr.responseText);
-        console.log(responseData)
+        console.log(responseData);
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
-      alert(xhr.status + ' ' + errorThrown + ': Server error. Please contact server administrator.');
+      alert(
+        xhr.status +
+          " " +
+          errorThrown +
+          ": Server error. Please contact server administrator."
+      );
     }
-  )
-}
+  );
+};
 
-setPostJsonEventToCreateEventBtn();
+setPostJsonEventToCreateEventbutton();
