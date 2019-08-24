0beec3942057a27eb7aeae93f312e2537e8657ed
JunYamaguchi <jun.smith.y@gmail.com>
2019-08-24 18:17:16 +0900

    add test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -8,6 +8,50 @@ from savoten import app, domain
     scope='function',
     params=[
         {
+            'uri': '/api/v1/events',
+            'expect': 200
+        }
+    ]
+)
+def get_events_test_case(request):
+    return request.param
+
+
+class TestGetEvents:
+    def setup_class(self):
+        uri = '/api/v1/events'
+        start = datetime.datetime.now()
+        end = start + datetime.timedelta(hours=1)
+        period_args = {
+            "start": start,
+            "end": end
+        }
+        period = domain.Period(**period_args)
+        event_args = {
+            "id": 1,
+            "name": "test_name",
+            "period": period,
+            "description": "test_desc",
+            "items": []
+        }
+        event = domain.Event(**event_args)
+        app.events.append(event)
+
+    def teardown_class(self):
+        app.events.clear()
+
+    def test_get_events(self, get_events_test_case):
+        uri = get_events_test_case['uri']
+        expect_status_code = get_events_test_case['expect']
+        test_app = app.api.test_client()
+        response = test_app.get(uri)
+        assert (response.status_code == expect_status_code)
+
+
+@pytest.fixture(
+    scope='function',
+    params=[
+        {
             # success case
             'uri': '/api/v1/events/1',
             'expect': 200
