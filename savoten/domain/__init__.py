153803ec7732402b35e5f674c0b188c160f142b6
JunYamaguchi <jun.smith.y@gmail.com>
2019-09-10 02:00:43 +0900

    add __init__.py

--- savoten/domain/__init__.py
+++ savoten/domain/__init__.py
@@ -3,3 +3,4 @@ from .event import Event  # noqa: F401
 from .event_item import EventItem  # noqa: F401
 from .period import Period  # noqa: F401
 from .user import User  # noqa: F401
+from .event_item_repository_interface import EventItemRepositoryInterface  # noqa: F401
