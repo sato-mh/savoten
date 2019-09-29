52e5655b1d6a9129929c186570f55a690bb27f49
佐藤雅宏 <sato-mh@sato-mh-macbook-pro.local>
2019-09-29 20:53:15 +0900

    Add error handler

--- savoten/handler/__init__.py
+++ savoten/handler/__init__.py
@@ -1,2 +1,2 @@
-from .event_api import EventAPI  # noqa F401
-from .event_view import event_view  # noqa F401
+from .event_api import EventAPI  # noqa: F401
+from .event_view import event_view  # noqa: F401
