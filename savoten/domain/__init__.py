c746eae1c2fd7c08e4c4b63f986ddadbb36e6bf8
conhumi <con_humi@live.jp>
2019-10-16 23:14:53 +0900

    Add EventRepositoryInterface to domain model

--- savoten/domain/__init__.py
+++ savoten/domain/__init__.py
@@ -1,5 +1,6 @@
 from .candidate import Candidate  # noqa: F401
 from .event import Event  # noqa: F401
 from .event_item import EventItem  # noqa: F401
+from .event_repository_interface import EventRepositoryInterface  # noqa: F401
 from .period import Period  # noqa: F401
 from .user import User  # noqa: F401
