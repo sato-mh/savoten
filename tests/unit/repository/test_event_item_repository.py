8ae542dbfa57c09fad87aa00b7c012dfdc7eb90d
JunYamaguchi <jun.smith.y@gmail.com>
2019-10-20 01:07:28 +0900

    apply linter&formatter

--- tests/unit/repository/test_event_item_repository.py
+++ tests/unit/repository/test_event_item_repository.py
@@ -13,6 +13,7 @@ user = User(**user_args)
 candidate_args = {'user': user}
 candidate = Candidate(**candidate_args)
 
+
 class TestSave:
 
     def setup_method(self, method):
@@ -27,19 +28,21 @@ class TestSave:
 
     @pytest.mark.parametrize('event_item',
                              [EventItem('test_name', [candidate], id=3)])
-    def test_update_succeeds_when_given_event_item_has_id(
-            self, event_item):
+    def test_update_succeeds_when_given_event_item_has_id(self, event_item):
         self.repository.save(event_item)
         event_item.name = 'updated_name'
         self.repository.save(event_item)
         assert get_public_vars(
             self.repository.event_items[3]) == get_public_vars(event_item)
 
+
 class TestDelete:
 
     def setup_method(self, method):
         self.repository = EventItemRepository()
-        self.repository.event_items[1] = [EventItem('test_name', [candidate], id=1)]
+        self.repository.event_items[1] = [
+            EventItem('test_name', [candidate], id=1)
+        ]
 
     @pytest.mark.parametrize('event_item',
                              [EventItem('test_name', [candidate], id=1)])
@@ -55,23 +58,28 @@ class TestDelete:
 
     @pytest.mark.parametrize('event_item',
                              [EventItem('test_name', [candidate], id=None)])
-    def test_return_value_error_when_given_event_item_id_is_none(self, event_item):
+    def test_return_value_error_when_given_event_item_id_is_none(
+            self, event_item):
         with pytest.raises(ValueError):
             assert self.repository.delete(event_item)
+
 
 class TestFindById:
 
     @classmethod
     def setup_class(cls):
         cls.repository = EventItemRepository()
-        cls.repository.event_items[1] = EventItem('test_name', [candidate], id=1)
+        cls.repository.event_items[1] = EventItem('test_name', [candidate],
+                                                  id=1)
 
     def test_return_event_item_when_target_id_exists(self):
-        assert get_public_vars(self.repository.find_by_id(1)) == get_public_vars(
-            self.repository.event_items[1])
+        assert get_public_vars(
+            self.repository.find_by_id(1)) == get_public_vars(
+                self.repository.event_items[1])
 
     def test_return_none_if_target_id_does_not_exist(self):
         assert self.repository.find_by_id(100) is None
+
 
 class TestFindByEventId:
 
@@ -83,11 +91,13 @@ class TestFindByEventId:
         cls.repository.event_id_to_event_item_map[event_id] = []
         for i in range(1, 5):
             event_item = EventItem('test_name', [candidate], id=i)
-            cls.repository.event_id_to_event_item_map[event_id].append(event_item)
+            cls.repository.event_id_to_event_item_map[event_id].append(
+                event_item)
             cls.added_event_items.append(event_item)
 
     def test_return_found_event_items_when_target_event_id_exists(self):
-        assert set(self.repository.find_by_event_id(1)) == set(self.added_event_items)
+        assert set(self.repository.find_by_event_id(1)) == set(
+            self.added_event_items)
 
     def test_return_none_when_target_event_item_does_not_exist(self):
         assert self.repository.find_by_event_id(2) is None
