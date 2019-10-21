c8ebe6f81cdf3b26c0731d7d9420f6b431834214
JunYamaguchi <jun.smith.y@gmail.com>
2019-10-21 13:49:01 +0900

    fix test

--- tests/unit/repository/test_candidate_repository.py
+++ tests/unit/repository/test_candidate_repository.py
@@ -12,115 +12,103 @@ user_args = {
 user = User(**user_args)
 
 
-@pytest.fixture(scope='function')
-def setup_repository():
-    candidate_repository = CandidateRepository()
-    yield (candidate_repository)
-    del (candidate_repository)
-
-
 class TestSave:
 
-    @pytest.mark.parametrize('candidate', [Candidate(user)])
-    def test_succeeds_when_candidate_has_no_id(self, candidate,
-                                               setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.save(candidate)
-        assert get_public_vars(
-            candidate_repository.candidates[1]) == get_public_vars(candidate)
+    def setup_method(self):
+        self.repository = CandidateRepository()
 
-    @pytest.mark.parametrize('candidate', [Candidate(user, id=3)])
-    def test_update_succeeds_when_candidate_has_id(self, candidate,
-                                                   setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.save(candidate)
-        candidate.name = 'updated_name'
-        candidate_repository.save(candidate)
+    @pytest.mark.parametrize('candidate', [Candidate(user)])
+    def test_succeeds_when_candidate_has_no_id(self, candidate):
+        self.repository.save(candidate)
         assert get_public_vars(
-            candidate_repository.candidates[3]) == get_public_vars(candidate)
+            self.repository.candidates[1]) == get_public_vars(candidate)
+
+    @pytest.mark.parametrize(
+        'candidate, updated_candidate',
+        [(Candidate(user), Candidate(user, id=1, description='updated'))])
+    def test_update_succeeds_when_candidate_has_id(self, candidate,
+                                                   updated_candidate):
+        self.repository.save(candidate)
+        self.repository.save(updated_candidate)
+        assert get_public_vars(
+            self.repository.candidates[1]) == get_public_vars(updated_candidate)
 
     @pytest.mark.parametrize('candidate, event_item_id',
                              [(Candidate(user, id=1), 1)])
     def test_save_succeeds_when_given_event_item_id(self, candidate,
-                                                    event_item_id,
-                                                    setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.save(candidate, event_item_id)
-        assert set(candidate_repository.
-                   event_item_id_to_candidate_map[event_item_id]) == set(
-                       [candidate])
+                                                    event_item_id):
+        self.repository.save(candidate, event_item_id)
+        assert set(self.repository.event_item_id_to_candidate_map[event_item_id]
+                  ) == set([candidate])
 
 
 class TestDelete:
 
-    @pytest.mark.parametrize('candidate', [Candidate(user, id=1)])
-    def test_succeeds_when_target_candidate_exists(self, candidate,
-                                                   setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.candidates[1] = candidate
-        candidate_repository.delete(candidate)
+    @classmethod
+    def setup_class(cls):
+        cls.repository = CandidateRepository()
+        cls.repository.candidates[1] = Candidate(user, id=1)
 
     @pytest.mark.parametrize('candidate', [Candidate(user, id=1)])
+    def test_succeeds_when_target_candidate_exists(self, candidate):
+        self.repository.delete(candidate)
+
+    @pytest.mark.parametrize('candidate', [Candidate(user, id=100)])
     def test_return_value_error_when_target_candidate_does_not_exist(
-            self, candidate, setup_repository):
-        candidate_repository = setup_repository
+            self, candidate):
         with pytest.raises(ValueError):
-            assert candidate_repository.delete(candidate)
+            assert self.repository.delete(candidate)
 
-    @pytest.mark.parametrize('candidate', [Candidate(user, id=1)])
+    @pytest.mark.parametrize('candidate', [Candidate(user, id=None)])
     def test_return_value_error_when_given_candidate_id_is_none(
-            self, candidate, setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.candidates[1] = candidate
-        candidate.id = None
+            self, candidate):
         with pytest.raises(ValueError):
-            assert candidate_repository.delete(candidate)
+            assert self.repository.delete(candidate)
 
 
 class TestFindById:
 
-    @pytest.mark.parametrize('candidate', [Candidate(user, id=1)])
-    def test_return_candidate_when_target_id_exists(self, candidate,
-                                                    setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.candidates[1] = candidate
-        found_candidate = candidate_repository.find_by_id(1)
-        assert get_public_vars(found_candidate) == get_public_vars(
-            candidate_repository.candidates[1])
+    @classmethod
+    def setup_class(cls):
+        cls.repository = CandidateRepository()
+        cls.repository.candidates[1] = Candidate(user, id=1)
 
-    def test_return_none_when_target_id_does_not_exist(self, setup_repository):
-        candidate_repository = setup_repository
-        found_candidate = candidate_repository.find_by_id(100)
-        assert found_candidate is None
+    def test_return_candidate_when_target_id_exists(self):
+        assert get_public_vars(
+            self.repository.find_by_id(1)) == get_public_vars(
+                self.repository.candidates[1])
+
+    def test_return_none_when_target_id_does_not_exist(self):
+        assert self.repository.find_by_id(100) is None
 
 
 class TestFindByEventItemId:
 
-    @pytest.mark.parametrize('candidate', [Candidate(user, id=1)])
-    def test_return_candidates_when_candidate_with_target_event_item_id_exists(
-            self, candidate, setup_repository):
-        candidate_repository = setup_repository
-        candidate_repository.candidates[1] = candidate
+    @classmethod
+    def setup_class(cls):
+        cls.repository = CandidateRepository()
+        cls.test_candidates = [Candidate(user, id=i) for i in range(1, 5)]
+        event_item_id = 1
+        cls.repository.event_item_id_to_candidate_map[
+            event_item_id] = cls.test_candidates
 
-        found_candidate = candidate_repository.find_by_id(1)
-        assert get_public_vars(found_candidate) == get_public_vars(
-            candidate_repository.candidates[1])
+    def test_return_candidates_when_target_event_ids_candidate_exists(self):
+        set(self.repository.find_by_event_item_id(1)) == set(
+            self.test_candidates)
 
-    def test_return_none_when_candidates_with_target_event_item_id_does_not_exists(
-            self, setup_repository):
-        candidate_repository = setup_repository
-        found_candidate = candidate_repository.find_by_id(100)
-        assert found_candidate is None
+    def test_return_none_when_target_event_item_ids_candidates_does_not_exists(
+            self):
+        assert self.repository.find_by_id(100) is None
 
 
 class TestFindAll:
 
-    def test_return_all_candidate(self, setup_repository):
-        candidate_repository = setup_repository
-        added_candidates = []
-        for id in range(1, 5):
-            candidate = Candidate(user, id)
-            candidate_repository.candidates[id] = candidate
-            added_candidates.append(candidate)
-        found_candidates = candidate_repository.find_all()
-        assert set(found_candidates) == set(added_candidates)
+    @classmethod
+    def setup_class(cls):
+        cls.repository = CandidateRepository()
+        cls.test_candidates = [Candidate(user, id=i) for i in range(1, 5)]
+        for candidate in cls.test_candidates:
+            cls.repository.candidates[candidate.id] = candidate
+
+    def test_return_all_candidate(self):
+        assert set(self.repository.find_all()) == set(self.test_candidates)
