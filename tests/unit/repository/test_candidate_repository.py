2b35776b590f0f176bd715ff58752dd770e9921b
JunYamaguchi <jun.smith.y@gmail.com>
2019-11-13 22:59:37 +0900

    fix lint error

--- tests/unit/repository/test_candidate_repository.py
+++ tests/unit/repository/test_candidate_repository.py
@@ -39,7 +39,7 @@ class TestSave:
                                                     event_item_id):
         self.repository.save(candidate, event_item_id)
         assert set(self.repository.event_item_id_to_candidate_map[event_item_id]
-                  ) == set([candidate])
+                  ) == set([candidate])  # noqa E124
 
 
 class TestDelete:
@@ -66,7 +66,8 @@ class TestDelete:
             self, candidate, event_item_id,
             regist_candidate_to_event_item_id_map):
         self.repository.delete(candidate)
-        for registed_candidates in self.repository.event_item_id_to_candidate_map.values():
+        for registed_candidates \
+                in self.repository.event_item_id_to_candidate_map.values():
             for registed_candidate in registed_candidates:
                 assert candidate.id == registed_candidate.id
 
