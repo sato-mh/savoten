676f865ee71c2a71e35629b1c5b3ef6b2a5ad7fe
JunYamaguchi <jun.smith.y@gmail.com>
2019-11-20 00:16:16 +0900

    fix noqa F401 in init.py

--- savoten/domain/__init__.py
+++ savoten/domain/__init__.py
@@ -1,7 +1,7 @@
-from .candidate import Candidate  # noqa: F401
-from .event import Event  # noqa: F401
-from .event_item import EventItem  # noqa: F401
-from .event_repository_interface import EventRepositoryInterface  # noqa: F401
+from .candidate import Candidate
+from .event import Event
+from .event_item import EventItem
+from .event_repository_interface import EventRepositoryInterface
 from .event_item_repository_interface import EventItemRepositoryInterface # noqa: F401
-from .period import Period  # noqa: F401
-from .user import User  # noqa: F401
+from .period import Period
+from .user import User
