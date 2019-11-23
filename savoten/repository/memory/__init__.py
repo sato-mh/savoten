9e1170badd191736e71917bbe506a7953cc10d84
JunYamaguchi <jun.smith.y@gmail.com>
2019-11-24 00:55:36 +0900

    add candidate_repository

--- savoten/repository/memory/__init__.py
+++ savoten/repository/memory/__init__.py
@@ -1,3 +1,4 @@
+from .candidate_repository import CandidateRepository
 from .event_item_repository import EventItemRepository
 from .event_repository import EventRepository
 from .user_repository import UserRepository
